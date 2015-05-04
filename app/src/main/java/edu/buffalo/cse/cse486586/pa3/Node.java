9f987ba4dc6d8d106dc1e1326d170c235773e42d
Jesus Sanchez <engineer.jesussanchez@gmail.com>
2015-05-04 13:21:56 -0400

    Second commit

---
+++ app/src/main/java/edu/buffalo/cse/cse486586/pa3/Node.java
@@ -0,0 +1,60 @@
+/*package edu.buffalo.cse.cse486586.pa3;
+
+public class Node {
+
+    private String id;
+    private Object data;
+
+    private Node predecessor;
+    private Node successor;
+
+    public Node() {}
+
+    public Node(String id) {
+        setId(id);
+    }
+
+    public Node(String id, Object data) {
+        setId(id);
+        setData(data);
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setPredecessor(Node predecessor) {
+        this.predecessor = predecessor;
+    }
+
+    public Node getPredecessor() {
+        return predecessor;
+    }
+
+    public void setSuccessor(Node successor) {
+        this.successor = successor;
+    }
+
+    public Node getSuccessor() {
+        return successor;
+    }
+
+    public boolean equals(Node node) {
+        return this.id.equals(node.id);
+    }
+
+}
+
+*/
